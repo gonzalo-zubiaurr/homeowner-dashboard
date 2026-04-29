fee034ce51178525bb7f46103998305444d8bfc1
gonzalo-zubiaurr <zubiaurr.victor@belonghome.com>
2026-04-29 15:51:13 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,7 +5,7 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     detectSessionInUrl: true,
-    flowType: 'implicit',
+    flowType: 'pkce',
     persistSession: true,
   }
 })
