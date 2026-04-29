fe62ce04226f5c4afb0281de261d69c1c4d3a42d
gonzalo-zubiaurr <zubiaurr.victor@belonghome.com>
2026-04-29 15:48:53 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,13 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    detectSessionInUrl: true,
+    flowType: 'implicit',
+    persistSession: true,
+  }
+})
 
 export type Lease = {
   lease_id: string
